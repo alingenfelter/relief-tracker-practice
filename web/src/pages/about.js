3a708efcdd44953d9c511368432c3ebcc9754e79
Andrea <alingenfelter2003@gmail.com>
2016-11-18 10:50:05 -0500

    step1: app-home-about

---
+++ web/src/pages/about.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const About = React.createClass({
+  render() {
+    return(
+      <div>
+        <h1>About the Relief Tracker</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = About
