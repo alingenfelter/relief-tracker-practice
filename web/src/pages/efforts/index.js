fb676487c1b2faf3444783016503032d53cc25d1
Andrea <alingenfelter2003@gmail.com>
2016-11-21 09:57:39 -0500

    add relief effort page

---
+++ web/src/pages/efforts/index.js
@@ -0,0 +1,34 @@
+const React = require('react')
+const {Link} = require('react-router')
+const xhr = require('xhr')
+
+const Efforts = React.createClass({
+  getInitialState: function() {
+    return{
+      efforts: []
+    }
+  },
+  componentDidMount() {
+    xhr.get('http://localhost:4000/efforts', {
+      json: true
+    }, (err, res, efforts) => {
+      if (err) return console.log(err.message)
+      this.setState({efforts})
+    })
+  },
+  render() {
+    const listEffort = effort =>
+      <li>{effort.name}</li>
+    return(
+      <div>
+        <h1>Relief Efforts</h1>
+        <ul>
+          {this.state.efforts.map(listEffort)}
+        </ul>
+        <Link to='/'>Home</Link>
+      </div>
+    )
+  }
+})
+
+module.exports = Efforts
